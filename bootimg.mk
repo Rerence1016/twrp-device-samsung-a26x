a9536ca9f1691e023b788fe5e856f0303ce21b5e
Rerence1016 <schp4544@gmail.com>
2025-04-28 18:40:16 +0800

    Change output name to `twrp-recovery-a26x.tar`

--- bootimg.mk
+++ bootimg.mk
@@ -1,6 +1,6 @@
 MKBOOTIMG := device/samsung/a26x/mkbootimg
 
-FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
+FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/twrp-recovery-a26x.tar
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
